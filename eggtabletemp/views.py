cbb41948ad85f08e39788f81673b6c46f7423d70
Muscle Hyeok <lsh96514@gmail.com>
2022-12-14 18:24:41 +0900

    테이블의 계란선별기 단락 작성

--- eggtabletemp/views.py
+++ eggtabletemp/views.py
@@ -7,3 +7,12 @@ def bootstrapTables(request):
   context['segment'] = load_template
   
   return render(request, "bootstrap-tables.html", context)
+
+
+def eggdetectTables(request):
+  context = {}
+
+  load_template = request.path.split('/')
+  context['segment'] = load_template
+
+  return render(request, "eggdetect-tables.html", context)
