e002b98655aa98730af51d4cacbd2bae43b9193c
cantabile26 <cantabile26@naver.com>
2022-12-15 10:22:46 +0900

    users 추가 및 db세팅

--- egg/settings.py
+++ egg/settings.py
@@ -42,11 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'main',
     'eggtemp',
     'eggpagetemp',
     'eggtabletemp',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
