c272265155b389df5e3dd0837d170b8f0b87b7e0
Junsoo Son <junsoo19970130@gmail.com>
2022-12-17 12:14:41 +0900

    모달에서 데이터 전달 설계 중

--- egg_info/models.py
+++ egg_info/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Egginfo(models.Model):
-    farm_code = models.BigIntegerField("농장코드")
-    barn_code = models.BigIntegerField("축사코드")
-    egg_code = models.BigAutoField(primary_key=True)
-    egg_registration_date = models.CharField(max_length=100, null=False)
-    egg_type = models.IntegerField()
-    egg_number = models.BigIntegerField()
+    farm_code = models.CharField("농장코드", max_length=50, null=False)
+    barn_code = models.CharField("축사코드", max_length=50, null=False)
+    egg_registration_date = models.DateTimeField("egg_registration_date", null=True)
+    egg_type = models.CharField("egg_type", max_length=50, null=False)
+    egg_number = models.BigIntegerField("egg_number", null=False)
--- egg_info/views.py
+++ egg_info/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+ 
+from .models import Egginfo
 
 # Create your views here.
 
@@ -18,9 +21,13 @@ def egg_data_upload_view(request):
   
   return render(request, "egg_data_upload.html", context)
 
-def egg_info_db(request):
-  egg_type = request.GET.get('egg_type')
-  context = {
-            'egg_type': egg_type,
-        }
-  return render(request, 'egg_data_upload.html', context)
+from django.shortcuts import render
+
+ 
+# Create your views here.
+def Egg_modal_data(request):
+    egg_modal_input = Egginfo.objects.all()
+    context = {'egg_modal_input':egg_modal_input}
+        #context에 모달에서 들어온 데이터를 저장
+    return render(request, 'egg_info.html', context)
+        #context안에 있는 모달 데이터를 egg_info.html로 전달
